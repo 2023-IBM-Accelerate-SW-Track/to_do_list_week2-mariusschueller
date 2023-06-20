caf86204e38363f4aa1650efe6e97cc7126fe001
Marius Schueller <mariuseschueller@gmail.com>
2023-06-19 19:14:31 -0500

    fixing small issue

--- src/component/todos.js
+++ src/component/todos.js
@@ -27,7 +27,7 @@ const Todos = ({ todos, deleteTodo }) => {
       );
     })
   ) : (
-    <p>You have no todos left</p>
+    <p>You have no todo's left</p>
   );
 
   return <div className="todoCollection" style={{ padding: "10px" }}>{todoList}</div>;
